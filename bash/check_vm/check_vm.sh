21619693dc087698e35c7f9d8030250caab2cd11
Goh'Dan <gohdan@gohdan.ru>
2015-01-14 17:04:26 +0300

    added more functionality including load average check through munin

--- bash/check_vm/check_vm.sh
+++ bash/check_vm/check_vm.sh
@@ -4,12 +4,25 @@ DIR=/opt/check_vm
 EXPECTSCRIPT="$DIR/telnet_script"
 TMPFILE=/tmp/check_vm_telnet_log.txt
 DELAY=10
+LA_THRESHOLD=90
 
 VMS=(
-	vm1
-	vm2
-	vm3
+        apache
 )
+
+function restart_vm()
+{
+        VM=$1
+
+        echo "trying to shutdown"
+        /usr/bin/virsh shutdown $VM
+        sleep $DELAY
+        echo "trying to destroy"
+        /usr/bin/virsh destroy $VM
+        sleep $DELAY
+        echo "trying to start"
+        /usr/bin/virsh start $VM
+}
 
 for VM in ${VMS[*]}; do
 
@@ -17,21 +30,43 @@ for VM in ${VMS[*]}; do
         HOST="vm-$VM"
         expect $EXPECTSCRIPT $HOST > $TMPFILE
 
+        #echo $results
+
 
         if [ "`grep Connected $TMPFILE`" ]
         then
                 #echo "$VM is up"
-                :
+                LA=`grep "load.value" $TMPFILE | awk '{print $2}' | awk -F . '{print $1}'`
+                #echo "LA: $LA"
+                if [ -n "$LA" ]
+                then
+                        if [ "$LA" -gt "$LA_THRESHOLD" ]
+                        then
+                                echo "$VM load average is too high: $LA"
+
+                                echo "=== Connection protocol ==="
+                                cat $TMPFILE
+                                echo "==========================="
+
+                                restart_vm $VM
+                        fi
+                else
+                        echo "Cannot get load average value, it seems that $VM is down"
+
+                        echo "=== Connection protocol ==="
+                        cat $TMPFILE
+                        echo "==========================="
+
+                        restart_vm $VM
+                fi
         else
                 echo "$VM is down"
-                echo "trying to shutdown"
-                /usr/bin/virsh shutdown $VM
-                sleep $DELAY
-                echo "trying to destroy"
-                /usr/bin/virsh destroy $VM
-                sleep $DELAY
-                echo "trying to start"
-                /usr/bin/virsh start $VM
+
+                echo "=== Connection protocol ==="
+                cat $TMPFILE
+                echo "==========================="
+
+                restart_vm $VM
         fi
 
         if [ -e $TMPFILE ]
@@ -41,3 +76,4 @@ for VM in ${VMS[*]}; do
 done
 
 exit 0
+
