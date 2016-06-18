9c091fbeb8c3db7b2a5606721a5478b49e13ff5f
Goh'Dan <gohdan@gohdan.ru>
2016-06-18 18:32:42 +0300

    Added new script

---
+++ bash/upload_most_recent_backups.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+HOST="ftp.example.org"
+USER="user"
+PASS="password"
+FTPURL="ftp://$USER:$PASS@$HOST"
+LCD="backup"
+RCD="backup"
+
+FILE1=`find $LCD/apache/ -type f -exec stat -c "%Y %n" {} \; | sort -n | head -1 | cut -d' ' -f2`
+
+FILE2=`find $LCD/mysql/ -type f -exec stat -c "%Y %n" {} \; | sort -n | head -1 | cut -d' ' -f2`
+
+
+FILES=( $FILE1 $FILE2 );
+
+for FILE in ${FILES[*]}; do
+
+lftp -c "set ftp:list-options -a;
+open '$FTPURL';
+cd $RCD;
+put $FILE;
+bye;"
+
+done
+
