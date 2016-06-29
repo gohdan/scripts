2c000d7dafb52d0aaf3aca8ae19d18cbc8f6f0c5
Goh'Dan <gohdan@gohdan.ru>
2016-06-29 20:20:32 +0300

    Improve file list building

--- bash/resize.sh
+++ bash/resize.sh
@@ -2,8 +2,18 @@
 # $1 - name of the subdir
 # $2 - width of the image
 
+ORIGIFS=$IFS
+ORIGOFS=$OFS;
+
+IFS=$(echo -en "\n\b")
+OFS=$(echo -en "\n\b")
+
 mkdir $1;
-for f in *; do
+for f in `find . -maxdepth 1 -type f`; do
 	echo "resizing "$f
 	convert -resize $2 "$f" "$1/${f%}" ;
 done
+
+IFS=$ORIGIFS
+OFS=$ORIGOFS
+
