d781421f0f61f916dad181ef7fa9317e7ff07549
Goh'Dan <gohdan@gohdan.ru>
2018-04-11 11:58:51 +0300

    Removed system databases from dump

--- mysql/dbs_dump.sh
+++ mysql/dbs_dump.sh
@@ -4,11 +4,19 @@ CURDATE=`date "+%Y%m%d-%H%M%S"`
 CLT=/usr/bin/mysql
 DUMP=/usr/bin/mysqldump
 BACKUPS=dumps
+IS="information_schema"
+PS="performance_schema"
 
 #for DBNAME in `cat dbs_2dump.txt`
 for DBNAME in `$CLT -h$SQLHOST --execute='show databases' -N -s `
 do
-echo 'dumping '$DBNAME
-$DUMP -R -E -h$SQLHOST $DBNAME >$BACKUPS/$DBNAME.sql
+if [ "$IS" != "$DBNAME" ]
+then
+        if [ "$PS" != "$DBNAME" ]
+        then
+			echo 'dumping '$DBNAME
+			$DUMP -R -E -h$SQLHOST $DBNAME >$BACKUPS/$DBNAME.sql
+		fi
+fi
 done
 
