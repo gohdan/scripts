6f0e21cafcd96d6b49e98e407bb3eb355cd286e0
Goh'Dan <gohdan@gohdan.ru>
2019-06-18 13:48:13 +0300

    Update script to use with user and password

--- mysql/tables_dump.sh
+++ mysql/tables_dump.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-#!/bin/sh
 SQLHOST=localhost
 CURDATE=`date "+%Y%m%d-%H%M%S"`
 CLT=/usr/bin/mysql
 DUMP=/usr/bin/mysqldump
-BACKUPS=dumps
+BACKUPS=db_tables
 DBNAME=db_name
+USER=user
+PASS=pass
 
+if [ ! -e ${BACKUPS} ]
+then
+	mkdir ${BACKUPS}
+fi
 
-for TBLNAME in `$CLT -h$SQLHOST $DBNAME --execute='show tables' -N -s `
+for TBLNAME in `${CLT} -h ${SQLHOST} ${DBNAME} -u ${USER} -p${PASS} --execute='show tables' -N -s`
 do
-
-	echo 'dumping '$TBLNAME
-	#$DUMP -R -E --skip-extended-insert -h$SQLHOST $DBNAME $TBLNAME >$BACKUPS/$TBLNAME.sql
-	$DUMP -R -E -h$SQLHOST $DBNAME $TBLNAME >$BACKUPS/$TBLNAME.sql
-
+	echo 'dumping '${TBLNAME}
+	#${DUMP} -R -E -h ${SQLHOST} ${DBNAME} ${TBLNAME} >${BACKUPS}/${TBLNAME}.sql
+	${DUMP} -R -E --skip-extended-insert -h ${SQLHOST} -u ${USER} -p${PASS} ${DBNAME} ${TBLNAME} >${BACKUPS}/${TBLNAME}.sql
 done
-
 
