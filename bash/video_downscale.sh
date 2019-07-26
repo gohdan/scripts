c1b83125d307db040837f3ea3e3a05331fa31619
Goh'Dan <gohdan@gohdan.ru>
2019-07-26 17:11:15 +0300

    Proper handling of whtespaces in filenames

--- bash/video_downscale.sh
+++ bash/video_downscale.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-mencoder $1 -o out.avi -of lavf -oac pcm -ovc lavc -lavcopts vcodec=mpeg4 -vf scale=480:270
+ORIGIFS=$IFS
+ORIGOFS=$OFS;
+
+IFS=$(echo -en "\n\b")
+OFS=$(echo -en "\n\b")
+
+mencoder ${1} -o out.avi -of lavf -oac pcm -ovc lavc -lavcopts vcodec=mpeg4 -vf scale=480:270
+
+IFS=$ORIGIFS
+OFS=$ORIGOFS
+
